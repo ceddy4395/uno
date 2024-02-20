3d49dbbc90d04a41c313bc4bf60519ce42e9113a
Jerome Laban <jerome@platform.uno>
2024-02-20 07:59:56 -0500

    chore: Adjust loaded for breadcrumb

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/Breadcrumb/BreadcrumbBar.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Breadcrumb/BreadcrumbBar.cs
@@ -706,15 +706,7 @@ public partial class BreadcrumbBar : Control
 	private protected override void OnLoaded()
 	{
 		base.OnLoaded();
-	}
 
-	private protected override void OnUnloaded()
-	{
-		base.OnUnloaded();
-	}
-
-	private void BreadcrumbBar_Loaded(object sender, RoutedEventArgs e)
-	{
 		if (_isUnloaded)
 		{
 			_isUnloaded = false;
@@ -722,8 +714,10 @@ public partial class BreadcrumbBar : Control
 		}
 	}
 
-	private void BreadcrumbBar_Unloaded(object sender, RoutedEventArgs e)
+	private protected override void OnUnloaded()
 	{
+		base.OnUnloaded();
+
 		RevokeListeners();
 		_isUnloaded = true;
 	}
